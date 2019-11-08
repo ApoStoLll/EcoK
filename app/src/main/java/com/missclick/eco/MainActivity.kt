d94849de0828fff00e38afd3edebbbf9565da557
Apostol <neketapostolov@gmail.com>
2019-11-08 14:30:20 +0200

    send "hellp"

--- app/src/main/java/com/missclick/eco/MainActivity.kt
+++ app/src/main/java/com/missclick/eco/MainActivity.kt
@@ -9,6 +9,10 @@ import java.io.PrintWriter
 import java.lang.Exception
 import java.net.Socket
 import kotlin.math.log
+import javax.xml.datatype.DatatypeConstants.SECONDS
+import android.os.AsyncTask
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,10 +21,16 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.navigation_home -> {
                 textMessage.setText(R.string.title_home)
+                //conn("Home")
+                val mt = MyTask()
+                mt.execute()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 textMessage.setText(R.string.title_dashboard)
+                //conn("DashBoard")
+                val mt = MyTask()
+                mt.execute()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
@@ -31,6 +41,21 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    fun conn(str: String){
+        Log.e("krash", "AFJHEFHIEUFHIEUFHNKEFNAIUEFNAUENFJKAE")
+        try{
+            val soc = Socket("95.158.11.238", 8080)
+            Log.e("krash", "Connect")
+            val writer = PrintWriter(soc.getOutputStream())
+            writer.write(str)
+            writer.flush()
+            writer.close()
+            soc.close()
+        }catch (e: Exception){
+            Log.e("krash", "bol" + e.toString())
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -38,17 +63,32 @@ class MainActivity : AppCompatActivity() {
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        Log.e("krash", "AFJHEFHIEUFHIEUFHNKEFNAIUEFNAUENFJKAE")
-        try{
-            val soc = Socket("192.168.0.103", 53210)
-            Log.e("krash", "Connect")
-            val writer = PrintWriter(soc.getOutputStream())
-            writer.write("kek")
-            writer.flush()
-            writer.close()
-            soc.close()
-        }catch (e: Exception){
-            Log.e("krash", "bol")
+
+    }
+    internal inner class MyTask : AsyncTask<Unit, Unit, Unit>() {
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+        }
+
+        override fun doInBackground(vararg params: Unit): Unit? {
+            try {
+                val soc = Socket("95.158.11.238", 8080)
+                Log.e("krash", "Connect")
+                val writer = PrintWriter(soc.getOutputStream())
+                writer.write("hellp")
+                writer.flush()
+                writer.close()
+                soc.close()
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+
+            return null
+        }
+
+        override fun onPostExecute(result: Unit?) {
+            super.onPostExecute(result)
         }
     }
 }
