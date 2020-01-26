de4bf9b005e3cd8f1428dbad0820a9551b3d5c42
danil.vladyka1 <danil.vladyka1@gmail.com>
2020-01-26 15:45:30 +0200

    Register Activity works,Carl!!!

--- app/src/main/java/com/missclick/eco/RegisterActivity.kt
+++ app/src/main/java/com/missclick/eco/RegisterActivity.kt
@@ -4,24 +4,41 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+
 import android.widget.Button
 
 
 class RegisterActivity : AppCompatActivity(){
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        //findViewById<Button>(R.id.button2).setOnClickListener({logIn()})
-        logi()
-
-
+        logInMenu()
     }
 
-     fun logi(){
+    private fun logInMenu(){
             val transaction = supportFragmentManager.beginTransaction()
             transaction.replace(R.id.fragment_holder,LogIn())
             transaction.addToBackStack(null)
             transaction.commit()
-        }
+    }
+
+    fun signUpMenu(){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.remove(LogIn())
+        transaction.replace(R.id.fragment_holder,SignUp())
+         transaction.addToBackStack(null)
+         transaction.commit()
+    }
+
+    fun logIn(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun signUp(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
 
 }
