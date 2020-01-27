09bb32a8211e63eb16d9bfcec734a441e06c631e
Apostol <neketapostolov@gmail.com>
2020-01-27 02:40:58 +0200

    comment input

--- app/src/main/java/com/missclick/eco/HttpClient.kt
+++ app/src/main/java/com/missclick/eco/HttpClient.kt
@@ -11,12 +11,12 @@ import android.system.Os.socket
 
 class HttpClient(val ip : String, val port : Int){
     var out : BufferedWriter
-    var input : BufferedReader
+    //var input : BufferedReader
     var soc : Socket
     init {
         soc = Socket(ip, port)
         out = BufferedWriter(OutputStreamWriter(soc.getOutputStream()))
-        input = BufferedReader(InputStreamReader(soc.getInputStream()))
+        //input = BufferedReader(InputStreamReader(soc.getInputStream()))
     }
     fun writeRequest(str : String, method: String){
         val requestLine = "$method $str HTTP/1.1\r\n"
@@ -28,9 +28,18 @@ class HttpClient(val ip : String, val port : Int){
         //val socket = Socket("192.168.0.135", 8080)
         out.write(request)
         out.flush()
-        Thread.sleep(1000)
-        //Log.e("RESPONSE: ", input.readLine())
-        //input.close()
         out.close()
+        //Log.e("RESPONSE: ", recvString(soc.getInputStream()))
+    }
+
+    @Throws(IOException::class)
+    fun recvString(input: InputStream): String {
+        var szBuf = ""
+        var ch = input.read()
+        while (ch >= 0 && ch != '\n'.toInt()) {
+            szBuf += ch.toChar()
+            ch = input.read()
+        }
+        return szBuf
     }
 }
