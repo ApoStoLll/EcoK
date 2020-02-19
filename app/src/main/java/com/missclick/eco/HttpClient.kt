d087f64c3a2d076f60605083507fcbbb1d4b8838
Apostol <neketapostolov@gmail.com>
2020-02-19 20:54:38 +0200

    AsyncTask change to coroutins and fix socket

--- app/src/main/java/com/missclick/eco/HttpClient.kt
+++ app/src/main/java/com/missclick/eco/HttpClient.kt
@@ -11,24 +11,30 @@ import android.system.Os.socket
 
 class HttpClient(val ip : String, val port : Int){
     var out : BufferedWriter
-    //var input : BufferedReader
+    var input : BufferedReader
     var soc : Socket
     init {
         soc = Socket(ip, port)
         out = BufferedWriter(OutputStreamWriter(soc.getOutputStream()))
-        //input = BufferedReader(InputStreamReader(soc.getInputStream()))
+        input = BufferedReader(InputStreamReader(soc.getInputStream()))
     }
     fun writeRequest(str : String, method: String){
         val requestLine = "$method $str HTTP/1.1\r\n"
         val host = "Host: $ip:$port\r\n"
         val request = requestLine + host
         write(request)
+        val message = input.readLine()
+        //out.close()
+        //input.close()
+        soc.close()
+        Log.e("RESPONSE: ", message)
     }
     fun write(request: String){
         //val socket = Socket("192.168.0.135", 8080)
+        Log.e("REQUEST: ", request)
         out.write(request)
         out.flush()
-        out.close()
+        soc.shutdownOutput()
         //Log.e("RESPONSE: ", recvString(soc.getInputStream()))
     }
 
--- app/src/main/java/com/missclick/eco/MainActivity.kt
+++ app/src/main/java/com/missclick/eco/MainActivity.kt
@@ -5,42 +5,51 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.TextView
+import kotlinx.coroutines.launch
 import java.io.PrintWriter
 import java.lang.Exception
 import java.net.Socket
 import kotlin.math.log
 import javax.xml.datatype.DatatypeConstants.SECONDS
 import android.os.AsyncTask
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
+
 
 //kek
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textMessage: TextView
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 textMessage.setText(R.string.title_home)
-                //conn("Home")
-                LolTask().execute(0)
+                conn(0)
+                //LolTask().execute(0)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 textMessage.setText(R.string.title_dashboard)
-                //conn("DashBoard")
-                LolTask().execute(1)
+                conn(1)
+                //LolTask().execute(1)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 textMessage.setText(R.string.title_notifications)
-                LolTask().execute(2)
+                //LolTask().execute(2)
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
 
-    fun conn(str: String){
+    /*fun conn(str: String){
         Log.e("krash", "AFJHEFHIEUFHIEUFHNKEFNAIUEFNAUENFJKAE")
         try{
             val soc = Socket("95.158.11.238", 8080)
@@ -52,6 +61,16 @@ class MainActivity : AppCompatActivity() {
             soc.close()
         }catch (e: Exception){
             Log.e("krash", "bol" + e.toString())
+        }
+    }*/
+
+    fun conn(code : Int){
+        GlobalScope.launch {
+            withContext(Dispatchers.IO){
+                val client = HttpClient("192.168.0.135", 8080)
+                client.writeRequest("0", "GET")
+            }
+            Log.e("COROUTINE", "DONE")
         }
     }
 
