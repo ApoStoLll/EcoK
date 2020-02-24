e8bc7ed58f5c1b46bb8c9d5c282ad8b905a9f19e
danil.vladyka1 <danil.vladyka1@gmail.com>
2020-02-24 18:44:10 +0200

    write in file

--- app/src/main/java/com/missclick/eco/RegisterActivity.kt
+++ app/src/main/java/com/missclick/eco/RegisterActivity.kt
@@ -1,14 +1,17 @@
 package com.missclick.eco
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.util.Log
 import kotlinx.android.synthetic.main.fragment_log_in.*
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.*
 
 
 class RegisterActivity : AppCompatActivity(){
@@ -44,6 +47,8 @@ class RegisterActivity : AppCompatActivity(){
                 //client.checkUser(nickname_logIn.text.toString(), password_logIn.text.toString())
             }
         }
+        writeFile(nickname_logIn.text.toString(), password_logIn.text.toString(),true)
+        readFile()
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         // переходим в мэин на свой акк
@@ -53,12 +58,58 @@ class RegisterActivity : AppCompatActivity(){
         GlobalScope.launch {
             withContext(Dispatchers.IO){
                 client.connect()
-                if(password_signUp.text.toString() == password_two.text.toString()) client.addUser(nickname_signUp.text.toString(),
-                    name.text.toString(),password_signUp.text.toString(), email.text.toString())
+                if(password_signUp.text.toString() == password_two.text.toString()) {
+                    writeFile(nickname_signUp.text.toString(), password_signUp.text.toString(),true)
+                    client.addUser(nickname_signUp.text.toString(),
+                        name.text.toString(),password_signUp.text.toString(), email.text.toString())
+                    }
+                }
             }
-        }
+
+
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    fun writeFile(nickname: String,password: String,isAuth: Boolean) {
+        try {
+            // отрываем поток для записи
+            val bw = BufferedWriter(
+                OutputStreamWriter(
+                    openFileOutput("fileName.txt", Context.MODE_PRIVATE)
+                )
+            )
+            // пишем данные
+            bw.write("$nickname\n$password\n$isAuth")
+            // закрываем поток
+            bw.close()
+            Log.d("Files: ", "Файл записан")
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun readFile() {
+        try {
+            // открываем поток для чтения
+            val br = BufferedReader(
+                InputStreamReader(
+                    openFileInput("fileName.txt")
+                )
+            )
+            var str = br.readLine()
+            // читаем содержимое
+            while (str != null) {
+                Log.d("Files", str)
+                str = br.readLine()
+            }
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
     }
 
 }
