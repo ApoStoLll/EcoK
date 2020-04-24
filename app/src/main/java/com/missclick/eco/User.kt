563ecc23bd04c1deec8b384eab0c41cc34e96020
danil.vladyka1 <danil.vladyka1@gmail.com>
2020-04-24 16:31:21 +0300

    delete profile adapter

--- app/src/main/java/com/missclick/eco/User.kt
+++ app/src/main/java/com/missclick/eco/User.kt
@@ -1,9 +1,4 @@
 package com.missclick.eco
 
-import android.graphics.Bitmap
-import android.media.Image
-import com.missclick.eco.main.profile.PositiveItem
-import com.missclick.eco.main.profile.ProfileItem
-
 data class User(val username : String, val name : String, val score : String, val imageName : String,
                 val followers : String = "", val following : String = "")
--- app/src/main/java/com/missclick/eco/main/profile/ProfileItem.kt
+++
@@ -1,6 +0,0 @@
-package com.missclick.eco.main.profile
-
-data class ProfileItem(
-    val firstName: String,
-    val lastName: String
-)
